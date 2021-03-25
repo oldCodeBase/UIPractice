650e309dee836d8a2be893c2c3c6131bd2fd2cb8
sageibra <akaev_ibragim@mail.ru>
2021-03-25 23:46:29 +0300

    Create a BrandLabel

---
+++ UIPractice/Custom Views/BrandLabel.swift
@@ -0,0 +1,26 @@
+//
+//  BrandLabel.swift
+//  UIPractice
+//
+//  Created by Ibragim Akaev on 25/03/2021.
+//
+
+import UIKit
+
+class BrandLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    convenience init(textColour: UIColor = .black, textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.textColor      = textColour
+        self.textAlignment  = textAlignment
+        self.font           = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
