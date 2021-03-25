76ea1740e95fd2185bf51e88cd7e863ca1e9b591
sageibra <akaev_ibragim@mail.ru>
2021-03-25 19:13:47 +0300

    Add constant file

---
+++ UIPractice/Extensions/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  UIPractice
+//
+//  Created by Ibragim Akaev on 25/03/2021.
+//
+
+import UIKit
+
+enum Colours {
+    static let brandColor   = UIColor(named: "brandColor")
+    static let brandGray    = UIColor(named: "brandGray")
+    static let brandGray2   = UIColor(named: "brandGray2")
+    static let brandGray3   = UIColor(named: "brandGray3")
+    static let brandGray4   = UIColor(named: "brandGray4")
+    static let brandGray5   = UIColor(named: "brandGray5")
+    static let brandGray6   = UIColor(named: "brandGray6")
+    static let brandSteal   = UIColor(named: "brandSteal")
+    static let brandBlack   = UIColor(named: "brandBlack")
+    static let brandWhite   = UIColor(named: "brandWhite")
+    static let brandPink    = UIColor(named: "brandPink")
+    static let brandOrange  = UIColor(named: "brandOrange")
+    static let brandBG      = UIColor(named: "brandBG")
+}
+
+enum Icons {
+    static let feeds            = UIImage(systemName: "hand.tap")
+    static let feedsSelected    = UIImage(systemName: "hand.tap.fill")
+    static let reports          = UIImage(systemName: "rectangle.stack")
+    static let reportsSelected  = UIImage(systemName: "rectangle.stack.fill")
+    static let user             = UIImage(systemName: "person.crop.circle")
+    static let userSelected     = UIImage(systemName: "person.crop.circle.fill")
+}
