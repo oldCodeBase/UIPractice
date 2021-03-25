b0d9a6cf32519ef37999291492489d1e6277225c
sageibra <akaev_ibragim@mail.ru>
2021-03-25 19:09:03 +0300

    Initial Commit

---
+++ UIPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIPractice
+//
+//  Created by Ibragim Akaev on 25/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
