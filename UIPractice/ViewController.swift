35feb5f4ebbe81996006eae1de378884c92d6785
sageibra <akaev_ibragim@mail.ru>
2021-03-25 19:12:55 +0300

    Update ViewController

--- UIPractice/ViewController.swift
+++ UIPractice/ViewController.swift
@@ -11,9 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
     }
+}
 
+import SwiftUI
 
+struct ViewControllerProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        let viewController = ViewController() // Write your VC name
+        func makeUIViewController(context: UIViewControllerRepresentableContext<ViewControllerProvider.ContainerView>) -> ViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: ViewControllerProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<ViewControllerProvider.ContainerView>) {
+            
+        }
+    }
 }
 
