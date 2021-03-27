58b58b9df4641b10ff8c9e44d0788e188ee5b313
sageibra <akaev_ibragim@mail.ru>
2021-03-27 23:58:39 +0300

    Create a FoundCell and update UI

---
+++ UIPractice/Screens/Founds/FoundCell.swift
@@ -0,0 +1,53 @@
+//
+//  FoundCell.swift
+//  UIPractice
+//
+//  Created by Ibragim Akaev on 27/03/2021.
+//
+
+import UIKit
+
+class FoundCell: UICollectionViewCell {
+    
+    static let reuseID              = "FoundCell"
+    private let foundLogoImageView  = UIImageView(image: Icons.foundLogo)
+    private let foundNameLabel      = BrandLabel(textAlignment: .left, fontSize: 17)
+    private let foundLocationLabel  = BrandLabel(textColor: Colors.brandGray, textAlignment: .left, fontSize: 13)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        foundLogoImageView.layer.cornerRadius = 4
+        self.backgroundColor = .white
+        self.layer.cornerRadius = 10
+        foundNameLabel.text = "Инсан"
+        foundNameLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        foundLocationLabel.text = "Ингушетия"
+        foundLocationLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        addSubviews(foundLogoImageView, foundNameLabel, foundLocationLabel)
+        
+        foundLogoImageView.snp.makeConstraints {
+            $0.height.width.equalTo(44)
+            $0.leading.equalToSuperview().offset(16)
+            $0.centerY.equalToSuperview()
+        }
+        
+        foundNameLabel.snp.makeConstraints {
+            $0.top.equalTo(foundLogoImageView)
+            $0.leading.equalTo(foundLogoImageView.snp.trailing).offset(12)
+        }
+        
+        foundLocationLabel.snp.makeConstraints {
+            $0.bottom.equalTo(foundLogoImageView)
+            $0.leading.equalTo(foundLogoImageView.snp.trailing).offset(12)
+        }
+    }
+}
